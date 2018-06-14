6c42c97a6480aaf954655ca719979123f64c0305
ELSANTOS <luissantos1231994@gmail.com>
2018-06-14 13:01:50 -0500

    Actualizacion

--- VERONICASBOUTIQUE/ListadeUsuarios.Designer.cs
+++ VERONICASBOUTIQUE/ListadeUsuarios.Designer.cs
@@ -30,15 +30,19 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btnPrincipal = new System.Windows.Forms.Button();
-            this.panel8 = new System.Windows.Forms.Panel();
             this.lblHora = new System.Windows.Forms.Label();
             this.lblFecha = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
             this.tmrFechaHora = new System.Windows.Forms.Timer(this.components);
+            this.txtCoincidir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPrincipal = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -51,6 +55,24 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(476, 37);
             this.panel1.TabIndex = 0;
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Font = new System.Drawing.Font("American Captain", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.Location = new System.Drawing.Point(334, 5);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(0, 29);
+            this.lblHora.TabIndex = 7;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("American Captain", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(24, 5);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(0, 29);
+            this.lblFecha.TabIndex = 6;
             // 
             // panel2
             // 
@@ -75,13 +97,42 @@
             this.label3.TabIndex = 42;
             this.label3.Text = "LISTA DE USUARIOS";
             // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel8.Location = new System.Drawing.Point(394, 0);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(80, 16);
+            this.panel8.TabIndex = 37;
+            // 
+            // tmrFechaHora
+            // 
+            this.tmrFechaHora.Enabled = true;
+            this.tmrFechaHora.Tick += new System.EventHandler(this.tmrFechaHora_Tick);
+            // 
+            // txtCoincidir
+            // 
+            this.txtCoincidir.Location = new System.Drawing.Point(103, 136);
+            this.txtCoincidir.Name = "txtCoincidir";
+            this.txtCoincidir.Size = new System.Drawing.Size(100, 20);
+            this.txtCoincidir.TabIndex = 57;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 58;
+            this.label1.Text = "Coincidir con:";
+            // 
             // btnPrincipal
             // 
             this.btnPrincipal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.btnPrincipal.FlatAppearance.BorderSize = 0;
             this.btnPrincipal.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.btnPrincipal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPrincipal.Image = global::VERONICASBOUTIQUE.Properties.Resources.HOME1;
+            this.btnPrincipal.Image = global::VERONICASBOUTIQUE.Properties.Resources.BACK;
             this.btnPrincipal.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
             this.btnPrincipal.Location = new System.Drawing.Point(394, 15);
             this.btnPrincipal.Name = "btnPrincipal";
@@ -91,36 +142,13 @@
             this.btnPrincipal.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnPrincipal.UseVisualStyleBackColor = false;
             // 
-            // panel8
+            // dataGridView1
             // 
-            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel8.Location = new System.Drawing.Point(394, 0);
-            this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(80, 16);
-            this.panel8.TabIndex = 37;
-            // 
-            // lblHora
-            // 
-            this.lblHora.AutoSize = true;
-            this.lblHora.Font = new System.Drawing.Font("American Captain", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHora.Location = new System.Drawing.Point(334, 5);
-            this.lblHora.Name = "lblHora";
-            this.lblHora.Size = new System.Drawing.Size(0, 29);
-            this.lblHora.TabIndex = 7;
-            // 
-            // lblFecha
-            // 
-            this.lblFecha.AutoSize = true;
-            this.lblFecha.Font = new System.Drawing.Font("American Captain", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFecha.Location = new System.Drawing.Point(24, 5);
-            this.lblFecha.Name = "lblFecha";
-            this.lblFecha.Size = new System.Drawing.Size(0, 29);
-            this.lblFecha.TabIndex = 6;
-            // 
-            // tmrFechaHora
-            // 
-            this.tmrFechaHora.Enabled = true;
-            this.tmrFechaHora.Tick += new System.EventHandler(this.tmrFechaHora_Tick);
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 171);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 233);
+            this.dataGridView1.TabIndex = 59;
             // 
             // ListadeUsuarios
             // 
@@ -130,6 +158,9 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(224)))), ((int)(((byte)(251)))));
             this.ClientSize = new System.Drawing.Size(476, 440);
             this.ControlBox = false;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCoincidir);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -139,7 +170,9 @@
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -153,5 +186,8 @@
         private System.Windows.Forms.Label lblHora;
         private System.Windows.Forms.Label lblFecha;
         private System.Windows.Forms.Timer tmrFechaHora;
+        private System.Windows.Forms.TextBox txtCoincidir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
